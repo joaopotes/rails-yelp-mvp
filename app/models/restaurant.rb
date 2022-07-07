c0d5ce47687d41007072d2d3320fd72aa5f69aa8
Joao Potes <joao.cpotes@gmail.com>
2022-07-07 19:10:23 +0100

    category solved

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 puts "cleaning up database"
 Restaurant.destroy_all
 puts "database is clean"
@@ -15,7 +16,8 @@ puts "creating restaurants..."
 restaurant = Restaurant.create(
   name: Faker::Restaurant.name,
   address: Faker::Address.street_address,
-  phone_number: Faker::PhoneNumber.cell_phone
+  phone_number: Faker::PhoneNumber.cell_phone,
+  category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
 )
 puts "restaurant #{restaurant.id} is created"
 end
